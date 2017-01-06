fac216b4a9821cebfd0ab8e26fdd8e66cf94d930
ChSandy <1513855069@qq,com$>
2017-01-06 16:23:11 +0800

    增加另一个二维码工具类

--- app/src/main/java/com/qf/colorerwerma/MainActivity.java
+++ app/src/main/java/com/qf/colorerwerma/MainActivity.java
@@ -1,15 +1,17 @@
 package com.qf.colorerwerma;
 
-import android.graphics.Bitmap;
+import android.content.Context;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.google.zxing.WriterException;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.ed);
+       /* editText = (EditText) findViewById(R.id.ed);
         imageView = (ImageView) findViewById(R.id.iv);
 
     }
@@ -51,5 +53,55 @@ public class MainActivity extends AppCompatActivity {
         // 得到新的图片   www.2cto.com
         Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
         return newbm;
+    }*/
+
+        //内容
+        final EditText contentET = (EditText) findViewById(R.id.ed);
+        //显示二维码图片
+        final ImageView imageView = (ImageView) findViewById(R.id.iv);
+        //是否添加Logo
+        final CheckBox addLogoCB = (CheckBox) findViewById(R.id.cb);
+        Button createQrBtn = (Button) findViewById(R.id.btn);
+
+        createQrBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final String filePath = getFileRoot(MainActivity.this) + File.separator
+                        + "qr_" + System.currentTimeMillis() + ".jpg";
+
+                //二维码图片较大时，生成图片、保存文件的时间可能较长，因此放在新线程中
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean success = QRCodeUtil.createQRImage(contentET.getText().toString().trim(), 800, 800,
+                                addLogoCB.isChecked() ? BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher01) : null,
+                                filePath);
+
+                        if (success) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imageView.setImageBitmap(BitmapFactory.decodeFile(filePath));
+                                }
+                            });
+                        }
+                    }
+                }).start();
+
+            }
+        });
+    }
+
+    //文件存储根目录
+    private String getFileRoot(Context context) {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File external = context.getExternalFilesDir(null);
+            if (external != null) {
+                return external.getAbsolutePath();
+            }
+        }
+
+        return context.getFilesDir().getAbsolutePath();
     }
 }
